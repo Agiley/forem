5b653ec44aa0c9177504c5d9c9f201954ed99329
Sebastian Johnsson <sebastian@agiley.se>
2015-03-09 12:41:29 +0100

    Make it possible to I18n.t forem_email/forem_name for NilUser

--- app/models/forem/nil_user.rb
+++ app/models/forem/nil_user.rb
@@ -1,11 +1,11 @@
 module Forem
   class NilUser < Forem.user_class
     def forem_email
-      "nobody@example.com"
+      I18n.t('forem.nil_user.email', default: "nobody@example.com")
     end
 
     def forem_name
-      "[deleted]"
+      I18n.t('forem.nil_user.username', default: "[deleted]")
     end
   end
 end
